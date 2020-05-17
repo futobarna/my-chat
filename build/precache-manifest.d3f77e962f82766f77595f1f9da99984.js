1521ee42a9875e09b0bd13839adfd38019f39855
futobarna <44547788+futobarna@users.noreply.github.com>
2020-05-17 14:44:03 +0200

    fixed gitignore

---
+++ build/precache-manifest.d3f77e962f82766f77595f1f9da99984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670eab5d6c7bdea30e1dd3e54637c21c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0270a61a2945662b0041",
+    "url": "./static/css/main.28011fb6.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0270a61a2945662b0041",
+    "url": "./static/js/main.4ee15e3e.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
